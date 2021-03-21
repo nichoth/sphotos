cdabeff9e815b4ab5c67d683c955068b2e5f84d8
nichoth <nichoth@gmail.com>
2021-03-20 22:29:19 -0700

    new error

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 require('./ssb-singleton-setup')
 const ssbSingleton = require('ssb-browser-core/ssb-singleton')
 
-var [err, SSB] = ssbSingleton.getSSB()
+ssbSingleton.getSSBEventually((err, SSB) => {
+    console.log('aaaaaaa', err, SSB)
 
-console.log('in herererer', err, SSB)
-
-SSB.whoami(function (err, info) {
-    console.log('in here', err, info)
+    SSB.whoami(function (err, info) {
+        console.log('in here', err, info)
+    })
 })
+
+
+// console.log('in herererer', err, SSB)
+
 
 ssbSingleton.onError(function (err) {
     console.log('errrrr', err)
 })
 ssbSingleton.onSuccess(function () {
-    console.log('success')
+    console.log('success', arguments)
 })
 
