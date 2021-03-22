38a42b98f09ac276315dd61a31dd263de6d85248
nichoth <nichoth@gmail.com>
2021-03-22 13:48:40 -0700

    get id

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const ssbSingleton = require('ssb-browser-core/ssb-singleton')
 // (isRelevantCB, resultCB)
 ssbSingleton.getSimpleSSBEventually(() => true, function (err, SSB) {
     console.log('aaaaaa', err, SSB)
+    console.log('**id**', SSB.net.id)
 })
 
 ssbSingleton.onError(function (err) {
