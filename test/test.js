ce04d9add50ed68b5efcc302a9a2a9d1a2bd36e1
nichoth <nichoth@gmail.com>
2021-03-27 13:29:31 -0700

    trying things

--- test/test.js
+++ test/test.js
@@ -17,6 +17,8 @@ var brStream = browserify(__dirname + '/example-app.js', {
     .pipe(browser)
 
 
+
+
 brStream 
     .once('data', function (ev) {
         // the first browser has started, now do the tests in `index.js`
@@ -25,11 +27,14 @@ brStream
             .pipe(tapeRun())
             .on('close', function (signal) {
                 console.log('tape-run close')
-                browser.end('console.log(location); window.close()');
+                // browser.end('console.log(location); window.close()')
+                // brStream.end('console.log(location); window.close()')
+                // brStream.destroy()
+                // browser.destroy()
             })
             // .on('results', console.log)
             .pipe(process.stdout)
     })
 .pipe(process.stdout)
  
-// browser.end('console.log(location); window.close()');
+browser.end('console.log(location); window.close()');
